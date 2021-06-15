d4d936a72898a4bb65d468fd87a0082fe1d0eb47
Rajat Salaria <salariarajat2@gmail.com>
2021-06-14 22:06:15 -0500

    array

---
+++ 36  median of sorted array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+	int t;  cin>>t;
+	while(t--){
+	    int n;  cin>>n;
+	    int sum=0,a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    sort(a,a+n);
+	    if(n%2==0){
+	        cout<<(a[n/2]+a[n/2-1])/2<<endl;
+	    }else{
+	        cout<<a[n/2]<<endl;
+	    }
+	}
+	return 0;
+}
