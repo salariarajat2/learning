4a25131aa148347f1c1ac6339724ff45081aad67
Rajat Salaria <salariarajat2@gmail.com>
2021-08-23 11:39:59 -0500

    Greedy

---
+++ 2 BFS.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Time Complexity: O(V+E), Space Complexity: O(V)
+
+void bfsOfGraph(int V, vector<int> adj[])
+{
+	vector<int> bfs;
+	vector<int> vis(V, 0);
+	queue<int> q;
+	q.push(0);
+	vis[0] = 1;
+	while (!q.empty())
+	{
+		int node = q.front();
+		q.pop();
+		bfs.push_back(node);
+
+		for (auto it : adj[node])
+		{
+			if (!vis[it])
+			{
+				vis[it] = 1;
+				q.push(it);
+			}
+		}
+	}
+	for (int i = 0; i < bfs.size(); i++)
+	{
+		cout << bfs[i] << " ";
+	}
+}
+
+int main()
+{
+
+	int V, E;
+	cin >> V >> E;
+
+	vector<int> adj[V + 1];
+
+	for (int i = 0; i < E; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u); // uncomment this for undirected graoh
+	}
+
+	bfsOfGraph(V, adj);
+
+	return 0;
+}
+
+/*
+
+8 10
+0 1
+0 2
+0 3
+0 4
+1 5
+2 5
+3 6
+4 6
+5 7
+6 7
+
+Output:
+0 1 2 3 4 5 6 7
+
+*/
