c470b7cabbc7987bc0a062f44791df53c596e6d8
Rajat Salaria <salariarajat2@gmail.com>
2021-10-29 14:59:28 +0530

    String

---
+++ 15 Balanced Parenthesis problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//TC = O(N), SC = O(N)
+
+int main()
+{
+    map<char, char> mp;
+    mp[')'] = '(';
+    mp[']'] = '[';
+    mp['}'] = '{';
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        stack<char> st;
+        cin >> s;
+        int flag = 0;
+        for (char b : s)
+        {
+            if (b == '(' || b == '{' || b == '[')
+                st.push(b);
+            else
+            {
+                if (!st.empty() and mp[b] == st.top())
+                    st.pop();
+                else
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+
+        if (!flag and st.empty())
+            cout << "YES" << endl;
+        else
+            cout << "NO " << endl;
+    }
+    return 0;
+}
